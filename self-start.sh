1bde4efba108fd5b1bf39e0bf2dc82e09196e67e
彼萌 <33471066+bimoe@users.noreply.github.com>
2019-03-22 05:05:29 +0800

    Update self-start.sh

--- self-start.sh
+++ self-start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Time: 2019-03-22
 
-echo "bash /root/shadowsocksr/logrun.sh" > /etc/rc.d/init.d/ssr-start
-chmod +x  /etc/rc.d/init.d/ssr-start
-echo "/etc/rc.d/init.d/ssr-start" >> /etc/rc.d/rc.local
-chmod +x /etc/rc.d/rc.local
+echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+echo 'bash /root/shadowsocks/run.sh' >> /etc/rc.local
+chmod +x /etc/rc.d/rc.local && chmod +x /etc/rc.local
 echo "开机启动设置完成！"
