8c540f8a3709ca5b36f28299be87a3ee78f65d0a
Bimoe <33471066+bimoe@users.noreply.github.com>
2019-03-22 19:15:53 +0800

    Update self-start.sh

--- self-start.sh
+++ self-start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Time: 2019-03-22
 
-echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
 echo 'bash /root/shadowsocks/run.sh' >> /etc/rc.local
 chmod +x /etc/rc.d/rc.local && chmod +x /etc/rc.local
-echo "开机启动设置完成！"
+echo "开机自启动设置完成！"
